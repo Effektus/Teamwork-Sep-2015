07d6b4d890ab1966451f27e23e417f654addd282
IskraNikolova <air_iskra@yahoo.com>
2015-09-28 21:19:01 +0300

    Write pseudo code

--- GameProject/GameTeamWork/GameTeamWork.cs
+++ GameProject/GameTeamWork/GameTeamWork.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 public class GameTeamWork
 {
+    //1.Structure "Object" contains four variables - coordinates, color, and its symbols.
+    struct Object
+    {
+        public int x;
+        public int y;
+        public ConsoleColor color;
+        public char symbol;
+    }
+
     public static void Main()
     {
-        //Hello!
+        //2.We will insert all in while-loop
+        while (true)
+        {
+            //3.Move our key pressed
+            //4.Move our object
+            //5.Check for other object are hitting
+            //6.Clear the console with (Console.Clear)
+            //7.Draw playfield
+            //8.Draw info
+            //9.Swol down program
+
+        }
     }
 }
 
