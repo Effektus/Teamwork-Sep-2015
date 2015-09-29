0918c63e3cbb1bd3c4dc10f97d2a7b460cc4e21e
Iskra Nikolova <air_iskra@yahoo.com>
2015-09-29 13:11:52 +0300

    Update GameTeamWork.cs

--- GameProject/GameTeamWork/GameTeamWork.cs
+++ GameProject/GameTeamWork/GameTeamWork.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 
@@ -7,27 +8,66 @@ public class GameTeamWork
     //1.Structure "Object" contains four variables - coordinates, color, and its symbols.
     struct Object
     {
-        public string x;
-        public string y;
+        public int x;
+        public int y;
         public ConsoleColor color;
         public char symbol;
     }
 
     public static void Main()
     {
-        //2.We will insert all in while-loop
+
+        //2.Draw playfield
+        int playfieldWidth = 9;
+        Console.BufferHeight = Console.WindowHeight = 30;//This is the size of the console window height.
+        Console.BufferWidth = Console.WindowWidth = 40;//size of window width.                              
+        Console.BackgroundColor = ConsoleColor.DarkRed;//color of playfield
+
+        //3.Make object
+        Object userObject = new Object();
+        userObject.x = 2;
+        userObject.y = Console.WindowHeight - 1;
+        userObject.symbol = '@';
+        userObject.color = ConsoleColor.Black;
+
+        //Random randomGenerator = new Random();
+        //List<Object> objects = new List<Object>();
+
+        //We will insert all in while-loop
         while (true)
         {
-            //3.Move our key pressed
             //4.Move our object
-            //5.Check for other object are hitting
-            //6.Clear the console with (Console.Clear)
-            //7.Draw playfield
-            Console.BufferHeight = Console.WindowHeight = 30;//This is the size of the console window height.
-            Console.BufferWidth = Console.WindowWidth = 40;//size of window width.
-            Console.BackgroundColor = ConsoleColor.Blue;//color of playfield
-            //8.Draw info
-            //9.Slow down program
+            while (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo pressedKey = Console.ReadKey(true);
+                if (pressedKey.Key == ConsoleKey.LeftArrow)
+                {
+                    if (userObject.x - 1 > 0)
+                    {
+                        userObject.x--;
+                    }
+                }
+                else if (pressedKey.Key == ConsoleKey.RightArrow)
+                {
+                    if (userObject.x + 1 < playfieldWidth)
+                    {
+                        userObject.x++;
+                    }
+                }
+            }
+
+            //6.Check for other object are hitting
+
+            //7.Clear the console with 
+            Console.Clear();
+
+            //8.print our object
+            PrintOnPosition(userObject.x, userObject.y, userObject.symbol, userObject.color);
+
+
+
+            //9.Draw info
+            //10.Slow down program
             Thread.Sleep(500);
 
         }
@@ -43,4 +83,7 @@ public class GameTeamWork
         Console.WriteLine(symbol);
     }
 }
+
+      
+  
 
