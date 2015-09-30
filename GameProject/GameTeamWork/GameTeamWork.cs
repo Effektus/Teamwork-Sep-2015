3975e08a118d57b788d2e834e5e89677716b5cc3
Iskra Nikolova <air_iskra@yahoo.com>
2015-09-30 10:50:28 +0300

    Update GameTeamWork.cs
    Add methods and documentation.

--- GameProject/GameTeamWork/GameTeamWork.cs
+++ GameProject/GameTeamWork/GameTeamWork.cs
@@ -5,127 +5,176 @@ using System.Threading;
 public class GameTeamWork
 {
     //1.Structure "Object" contains four variables - coordinates, color, and its symbols.
-    class Object
+    public class Object
     {
         public int x;
         public int y;
         public ConsoleColor color;
-        public string symbol;
+        public char symbol;
     }
 
+    private static readonly Random RandomGenerator = new Random();
+    private static int playfieldWidth = 9;
     public static void Main()
     {
         int livesCount = 6;
-//2.Draw playfield
-        int playfieldWidth = 9;
-        Console.BufferHeight = Console.WindowHeight = 30;//This is the size of the console window height.
-        Console.BufferWidth = Console.WindowWidth = 40;//size of window width.                              
-        Console.BackgroundColor = ConsoleColor.Black;//color of playfield
-//3.Make object
-        Object userObject = new Object();
-        userObject.x = 2;
-        userObject.y = Console.WindowHeight - 1;
-        userObject.symbol = "@";
-        userObject.color = ConsoleColor.DarkRed;
+        int point = 0;
+        double speed = 100.0;
+        double acceleration = 0.5;
+
+        //2.Draw playfield
+        DrawPlayfield();
+
+        //3.Make user object
+        var userObject = MakeUserObject();
 
         List<Object> objects = new List<Object>();
-        Random randomGenerator = new Random();
         while (true)
         {
+            speed += acceleration;
+            if (speed > 400)
+            {
+                speed = 400;
+            }
             bool hit = false;
-            int chance = randomGenerator.Next(0, 100);
+            int chance = RandomGenerator.Next(0, 100);
 
             if (chance < 50)
             {
-                Object newObject = new Object();
-                newObject.color = ConsoleColor.Green;
-                newObject.x = randomGenerator.Next(0, playfieldWidth);
-                newObject.y = 0;
-                newObject.symbol = "$";
-                objects.Add(newObject);
+               MakeObjects(objects, '$', ConsoleColor.Green);
             }
 
-//4.Move our object
-            while (Console.KeyAvailable)
-            {
-                ConsoleKeyInfo pressedKey = Console.ReadKey(true);
-                while (Console.KeyAvailable)
-                {
-                    Console.ReadKey(true);
-                }
-                if (pressedKey.Key == ConsoleKey.LeftArrow)
-                {
-                    if (userObject.x  > 0)
-                    {
-                        userObject.x--;
-                    }
-                }
-                else if (pressedKey.Key == ConsoleKey.RightArrow)
-                {
-                    if (userObject.x + 1 < playfieldWidth)
-                    {
-                        userObject.x++;
-                    }
-                }
-            }
-//Struct was converted to Class , so now we can change (y) easily!
+            //4.Move our object
+            PressedKey(userObject, playfieldWidth);
+
+            //Struct was converted to Class , so now we can change (y) easily!
             for (int i = 0; i < objects.Count; i++)
             {
-                if (objects[i].y < Console.WindowHeight)
-                {
-                    objects[i].y = objects[i].y + 1;
-                }
-            }
- // Check for HIT!!!
-            if (objects.Count > 0)
-            {
-                if (objects[0].y == userObject.y && objects[0].x == userObject.x)
+                //is hit?
+                if (objects[0].symbol == '$' && objects[0].y == userObject.y && objects[0].x == userObject.x)
                 {
                     livesCount--;
+                    hit = true;
+                    Console.Beep(658, 125); Console.Beep(1320, 500);//sound
                 }
-            }
-//7.Clear the console with 
-            Console.Clear();
-//Print other object
-            foreach (Object element in objects)
-            {
-                PrintOnPosition(element.x, element.y, element.symbol, element.color);
-            }
-//8.print our object
-            PrintOnPosition(userObject.x, userObject.y, userObject.symbol, userObject.color);
-//printing userObject lives
-
-            PrintOnPosition(15, 25,"Lives:"+livesCount.ToString(),ConsoleColor.DarkRed);
-            //Print board for the field
-            PrintTheSideBoard();
-            //remove objects which are outside the field
-            if (objects.Count>0)
-            {
                 if (objects[0].y == Console.WindowHeight - 1)
                 {
                     objects.RemoveAt(0);
                 }
+                if (objects[i].y < Console.WindowHeight - 1)
+                {
+                    objects[i].y = objects[i].y + 1;
+                }
+
             }
+   
+            Console.Clear();
+
+            foreach (Object element in objects)
+            {
+                PrintOnPosition(element.x, element.y, element.symbol, element.color);
+            }
+
+            PrintOnPosition(userObject.x, userObject.y, userObject.symbol, userObject.color);
+
             //9.Draw info
-            //10.Slow down program
-            Thread.Sleep(300);
+
+            Thread.Sleep(500);
         }
     }
 
-    private static void PrintTheSideBoard()
+    private static void PressedKey(Object userObject, int playfieldWidth)
     {
-        for (int i = 0; i < Console.WindowHeight; i++)
+        while (Console.KeyAvailable)
         {
-            PrintOnPosition(9, i, "|", ConsoleColor.White);
+            ConsoleKeyInfo pressedKey = Console.ReadKey(true);
+            while (Console.KeyAvailable)
+            {
+                Console.ReadKey(true);
+            }
+            if (pressedKey.Key == ConsoleKey.LeftArrow)
+            {
+                if (userObject.x - 1 > 0)
+                {
+                    userObject.x--;
+                }
+            }
+            else if (pressedKey.Key == ConsoleKey.RightArrow)
+            {
+                if (userObject.x + 1 < playfieldWidth)
+                {
+                    userObject.x++;
+                }
+            }
         }
     }
+    /// <summary>
+    /// Make user player
+    /// </summary>
+    /// <returns></returns>
+    private static Object MakeUserObject()
+    {
+        Object userObject = new Object();
+        userObject.x = 2;
+        userObject.y = Console.WindowHeight - 1;
+        userObject.symbol = '@';
+        userObject.color = ConsoleColor.DarkRed;
+        return userObject;
+    }
 
-    //Method which print object 
-    static void PrintOnPosition(int x, int y, string symbol, ConsoleColor color = ConsoleColor.Green)
+    /// <summary>
+    /// Objects generator.
+    /// </summary>
+    /// <param name="objects">list of object where we added</param>
+    /// <param name="symbol">Symbol of object</param>
+    /// <param name="color">Color of object</param>
+    private static void MakeObjects(List<Object> objects, char symbol, ConsoleColor color)
+    {
+        Object newObject = new Object();
+        newObject.color = color;
+        newObject.x = RandomGenerator.Next(0, playfieldWidth);
+        newObject.y = 0;
+        newObject.symbol = symbol;
+        objects.Add(newObject);
+    }
+
+    /// <summary>
+    /// Draw play field
+    /// </summary>
+    private static void DrawPlayfield()
+    {
+        Console.BufferHeight = Console.WindowHeight = 30; //This is the size of the console window height.
+        Console.BufferWidth = Console.WindowWidth = 40; //size of window width.                              
+        Console.BackgroundColor = ConsoleColor.Black; //color of playfield
+    }
+
+    /// <summary>
+    /// Print object on position
+    /// </summary>
+    /// <param name="x">position x</param>
+    /// <param name="y">position y</param>
+    /// <param name="symbol">Symbol of player</param>
+    /// <param name="color">Color of player</param>
+    private static void PrintOnPosition(int x, int y, char symbol, ConsoleColor color = ConsoleColor.Green)
     {
         //Console.SetCursorPosition move our cursor in place of what we write.
         Console.SetCursorPosition(x, y);
         Console.ForegroundColor = color;
         Console.Write(symbol);
+    }
+
+    /// <summary>
+    /// Print information
+    /// </summary>
+    /// <param name="x">position x of info</param>
+    /// <param name="y">position y info</param>
+    /// <param name="str">text</param>
+    /// <param name="color">color of text</param>
+    private static void PrintStringOnPosition(int x, int y, string str,
+     ConsoleColor color = ConsoleColor.DarkCyan) //Draw info 
+    {
+        Console.SetCursorPosition(x, y);
+        Console.ForegroundColor = color;
+        Console.WriteLine(str);
     }
 }
