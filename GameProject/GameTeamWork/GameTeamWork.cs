8fab4ce48ce1104b446df92030d122f1db43945a
Stiliyan <stilian.kocev@gmail.com>
2015-09-30 10:02:51 +0300

    Printing method changed to work with string,now possible to be used to print LIVES and other data!  Some bugs were fixed!

--- GameProject/GameTeamWork/GameTeamWork.cs
+++ GameProject/GameTeamWork/GameTeamWork.cs
@@ -10,22 +10,22 @@ public class GameTeamWork
         public int x;
         public int y;
         public ConsoleColor color;
-        public char symbol;
+        public string symbol;
     }
 
     public static void Main()
     {
         int livesCount = 6;
-        //2.Draw playfield
+//2.Draw playfield
         int playfieldWidth = 9;
         Console.BufferHeight = Console.WindowHeight = 30;//This is the size of the console window height.
         Console.BufferWidth = Console.WindowWidth = 40;//size of window width.                              
         Console.BackgroundColor = ConsoleColor.Black;//color of playfield
-        //3.Make object
+//3.Make object
         Object userObject = new Object();
         userObject.x = 2;
         userObject.y = Console.WindowHeight - 1;
-        userObject.symbol = '@';
+        userObject.symbol = "@";
         userObject.color = ConsoleColor.DarkRed;
 
         List<Object> objects = new List<Object>();
@@ -41,11 +41,11 @@ public class GameTeamWork
                 newObject.color = ConsoleColor.Green;
                 newObject.x = randomGenerator.Next(0, playfieldWidth);
                 newObject.y = 0;
-                newObject.symbol = '$';
+                newObject.symbol = "$";
                 objects.Add(newObject);
             }
 
-            //4.Move our object
+//4.Move our object
             while (Console.KeyAvailable)
             {
                 ConsoleKeyInfo pressedKey = Console.ReadKey(true);
@@ -55,7 +55,7 @@ public class GameTeamWork
                 }
                 if (pressedKey.Key == ConsoleKey.LeftArrow)
                 {
-                    if (userObject.x - 1 > 0)
+                    if (userObject.x  > 0)
                     {
                         userObject.x--;
                     }
@@ -68,63 +68,50 @@ public class GameTeamWork
                     }
                 }
             }
-            //Struct was converted to Class , so now we can change (y) easily!
+//Struct was converted to Class , so now we can change (y) easily!
             for (int i = 0; i < objects.Count; i++)
             {
-                if (objects[0].y == Console.WindowHeight-1)
-                {
-                    objects.RemoveAt(0);
-                }
-                if (objects[i].y < Console.WindowHeight-1)
+                if (objects[i].y < Console.WindowHeight)
                 {
                     objects[i].y = objects[i].y + 1;
                 }
-
             }
-            //List<object> newList = new List<object>();
-            //for (int i = 0; i < objects.Count; i++)
-            //{
-            //    Object oldObject = objects[i];
-            //    Object changeObject = new Object();
-            //    changeObject.x = oldObject.x;
-            //    changeObject.y = oldObject.y + 1;
-            //    changeObject.symbol = oldObject.symbol;
-            //    objects.Remove(oldObject);
-            //    objects.Add(changeObject);
-            //    if (changeObject.symbol == '#' && changeObject.y == userObject.y && changeObject.x == userObject.x)
-            //    //Check if other cars are hitting us 3
-            //    {
-            //        livesCount--;
-            //        //hit = true;
-            //    }
-
-            //    if (changeObject.y < Console.WindowHeight)
-            //    {
-            //        newList.Add(changeObject);
-            //    }
-            //}
-
-
-            //6.Check for other object are hitting
-            
-            //7.Clear the console with 
+ // Check for HIT!!!
+            if (objects.Count > 0)
+            {
+                if (objects[0].y == userObject.y && objects[0].x == userObject.x)
+                {
+                    livesCount--;
+                }
+            }
+//7.Clear the console with 
             Console.Clear();
-            //Print other object
-
+//Print other object
             foreach (Object element in objects)
             {
                 PrintOnPosition(element.x, element.y, element.symbol, element.color);
             }
+//8.print our object
             PrintOnPosition(userObject.x, userObject.y, userObject.symbol, userObject.color);
-            //8.print our object
+//printing userObject lives
 
+            PrintOnPosition(15, 25,"Lives:"+livesCount.ToString(),ConsoleColor.DarkRed);
+
+//remove objects which are outside the field
+            if (objects.Count>0)
+            {
+                if (objects[0].y == Console.WindowHeight - 1)
+                {
+                    objects.RemoveAt(0);
+                }
+            }
             //9.Draw info
             //10.Slow down program
-            Thread.Sleep(500);
+            Thread.Sleep(300);
         }
     }
-    //Method which print object 
-    static void PrintOnPosition(int x, int y, char symbol, ConsoleColor color = ConsoleColor.Green)
+//Method which print object 
+    static void PrintOnPosition(int x, int y, string symbol, ConsoleColor color = ConsoleColor.Green)
     {
         //Console.SetCursorPosition move our cursor in place of what we write.
         Console.SetCursorPosition(x, y);
